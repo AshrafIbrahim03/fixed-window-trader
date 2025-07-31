cef6c4b385e2932a1a14221860d8cd118de26839
Ashraf Ibrahim <btd2eb@virginia.edu>
2025-07-31 15:01:32 -0400

    Added multiple stock functionality

--- main.go
+++ main.go
@@ -16,10 +16,13 @@ import (
 type (
 	DataClient interface {
 		// gets data from the current time minus the time window to the current time
-		GetData(time_window uint64) []Dollar
+		GetTrades(ticket string) ([]Dollar, error)
+		// gets the window that the data is on. It starts at the current time minus the time window
+		// to the current time
+		GetTimeWindow() time.Duration
 	}
 	TradeClient interface {
-		//Makes a trade based on current data
+		// Makes a trade based on current data
 		Trade() error
 
 		// Returns whether the market is open or not
@@ -34,11 +37,14 @@ type RunningMeanDataClient struct {
 	meanPeriods time.Duration // the time each average is done over
 }
 
+func (rmdc *RunningMeanDataClient) GetTimeWindow() time.Duration {
+	return rmdc.timeWindow
+}
+
 // Returns the trades from the specified time_window
 // time_window is the amount of time from the present the window goes into the past
 // ticker is the string ticker to be getting info for
 func (rmdc *RunningMeanDataClient) GetTrades(ticker string) ([]Dollar, error) {
-
 	trades, err := rmdc.dataClient.GetTrades(ticker, marketdata.GetTradesRequest{Start: time.Now().Add(-rmdc.timeWindow), End: time.Now()})
 	if err != nil {
 		return nil, err
@@ -49,7 +55,7 @@ func (rmdc *RunningMeanDataClient) GetTrades(ticker string) ([]Dollar, error) {
 	})
 
 	runningMeans := make([]Dollar, 0)
-	//runningTotal := float64(0)
+	// runningTotal := float64(0)
 
 	for i := range trades {
 		windowStart := trades[i].Timestamp
@@ -70,7 +76,7 @@ func (rmdc *RunningMeanDataClient) GetTrades(ticker string) ([]Dollar, error) {
 			runningMeans = append(runningMeans, Dollar(sum/float64(count)))
 		}
 	}
-	//fmt.Println("this array should be in ascending order", trades)
+	// fmt.Println("this array should be in ascending order", trades)
 
 	return runningMeans, nil
 }
@@ -84,13 +90,12 @@ type SlopeTarget struct {
 type FixedWindowTrader struct {
 	symbol      string
 	slopeTarget SlopeTarget
-	dataClient  *RunningMeanDataClient
+	dataClient  DataClient
 	tradeClient *alpaca.Client
 }
 
-func NewFixedWindowTrader(symbol string, slopeTarget SlopeTarget, dataClient *RunningMeanDataClient, tradeClient *alpaca.Client) (*FixedWindowTrader, error) {
-
-	if slopeTarget.timeScale != dataClient.timeWindow {
+func NewFixedWindowTrader(symbol string, slopeTarget SlopeTarget, dataClient DataClient, tradeClient *alpaca.Client) (*FixedWindowTrader, error) {
+	if slopeTarget.timeScale != dataClient.GetTimeWindow() {
 		// Figure out how to use any slope across any time scale
 		errors.New("timescale and timeWindow should be the same. I need to figure out a solution later")
 	}
@@ -105,18 +110,14 @@ func NewFixedWindowTrader(symbol string, slopeTarget SlopeTarget, dataClient *Ru
 
 func (fwt *FixedWindowTrader) IsMarketOpen() (bool, error) {
 	clock, err := fwt.tradeClient.GetClock()
-
 	if err != nil {
 		return false, errors.New(fmt.Sprint("Could not get clock, error: ", err))
 	}
 	return clock.IsOpen, nil
-
 }
 
 func (fwt *FixedWindowTrader) waitForMarket() error {
-
 	clock, err := fwt.tradeClient.GetClock()
-
 	if err != nil {
 		return errors.New(fmt.Sprint("Could not get clock, error: ", err))
 	}
@@ -125,7 +126,6 @@ func (fwt *FixedWindowTrader) waitForMarket() error {
 
 	time.Sleep(timeUntilOpen)
 	return nil
-
 }
 
 func (fwt *FixedWindowTrader) Trade() (*alpaca.Order, error) {
@@ -145,7 +145,7 @@ func (fwt *FixedWindowTrader) Trade() (*alpaca.Order, error) {
 	if len(recent_trades) == 0 {
 		return nil, errors.New("No trades are received")
 	}
-	current_slope := (recent_trades[len(recent_trades)-1] - recent_trades[0]) / Dollar(fwt.dataClient.timeWindow.Seconds())
+	current_slope := (recent_trades[len(recent_trades)-1] - recent_trades[0]) / Dollar(fwt.dataClient.GetTimeWindow().Seconds())
 	fmt.Println("current slope is", current_slope)
 	acc, a_err := fwt.tradeClient.GetAccount()
 	if a_err != nil {
@@ -153,7 +153,7 @@ func (fwt *FixedWindowTrader) Trade() (*alpaca.Order, error) {
 	}
 
 	if current_slope >= Dollar(fwt.slopeTarget.target) && acc.BuyingPower.GreaterThanOrEqual(decimal.Zero) {
-		//buy stonks if possible
+		// buy stonks if possible
 		if acc.BuyingPower.LessThanOrEqual(decimal.NewFromInt(1)) {
 			return nil, nil
 			fmt.Println("Not enough buying power to place an order on", fwt.symbol)
@@ -167,7 +167,7 @@ func (fwt *FixedWindowTrader) Trade() (*alpaca.Order, error) {
 
 		return order, nil
 	}
-	//sell stonks
+	// sell stonks
 	position, p_err := fwt.tradeClient.GetPosition(fwt.symbol)
 	if p_err != nil {
 		// If error is 404 (position does not exist), it's not really an error for us
@@ -189,51 +189,15 @@ func (fwt *FixedWindowTrader) Trade() (*alpaca.Order, error) {
 	return order, nil
 }
 
-func main() {
-	env_err := godotenv.Load()
-	if env_err != nil {
-	}
-	apca_key := os.Getenv("APCA_API_KEY_ID")
-	apca_secret := os.Getenv("APCA_API_SECRET_KEY")
-	base_url := os.Getenv("APCA_API_BASE_URL")
-	//fmt.Println("apca_key:", apca_key, "apca_secret:", apca_secret) //, "base_url:", base_url)
-	TIME_WINDOW := 7 * 24 * time.Hour
-	NUM_TIMES_TRADE_PER_WINDOW := 4.0
-	time_between_trades := TIME_WINDOW.Seconds() / NUM_TIMES_TRADE_PER_WINDOW
-	time_scale := time.Second
-
-	data_client := marketdata.NewClient(marketdata.ClientOpts{
-		APIKey:    apca_key,
-		APISecret: apca_secret,
-		//BaseURL:   base_url,
-		Feed: marketdata.IEX,
-	})
-	trade_client := alpaca.NewClient(alpaca.ClientOpts{
-		APIKey:    apca_key,
-		APISecret: apca_secret,
-		BaseURL:   base_url,
-	})
-	rmdc := RunningMeanDataClient{dataClient: data_client, meanPeriods: 10 * time.Minute, timeWindow: TIME_WINDOW}
-
-	//trades, rmdc_err := rmdc.GetTrades("AAPL")
-	//if rmdc_err != nil {
-	//		fmt.Println("There's an error:")
-	//		fmt.Println(rmdc_err)
-	//	}
-	//	fmt.Println(trades)
-
-	fwt, fwt_err := NewFixedWindowTrader("TSLA", SlopeTarget{target: 0, timeScale: TIME_WINDOW}, &rmdc, trade_client)
-	if fwt_err != nil {
-		fmt.Println(fwt_err)
-	}
+func RunFixedWindowTrader(fwt *FixedWindowTrader) {
 	for {
-		market_waiting_err := fwt.waitForMarket()
-		if market_waiting_err != nil {
-			fmt.Println(market_waiting_err)
+		marketWaitingErr := fwt.waitForMarket()
+		if marketWaitingErr != nil {
+			fmt.Println(marketWaitingErr)
 		}
-		order, order_err := fwt.Trade()
-		if order_err != nil {
-			fmt.Println("order error:", order_err)
+		order, orderErr := fwt.Trade()
+		if orderErr != nil {
+			fmt.Println("order error:", orderErr)
 		}
 		if order != nil {
 			fmt.Println(time.Now(), order.Side, order.FilledQty, "orders of ", order.Symbol, "at a price of", order.Notional)
@@ -241,9 +205,67 @@ func main() {
 			fmt.Println(time.Now(), "No trade made")
 		}
 
-		time_to_sleep := time.Duration(time_between_trades) * time_scale
-		fmt.Println("Sleeping for", time_to_sleep.Seconds(), "seconds")
-		time.Sleep(time_to_sleep)
+		timeToSleep := time.Duration(timeBetweenTrades) * timeScale
+		fmt.Println("Sleeping for", timeToSleep.Seconds(), "seconds")
+		time.Sleep(timeToSleep)
+	}
+}
+
+var (
+	exitChannel                = make(chan bool)
+	TimeWindow                 = 7 * 24 * time.Hour
+	NUM_TIMES_TRADE_PER_WINDOW = 4.0
+	TICKERS                    = []string{"TSLA", "AAPL", "NVDA", "PLTR", "MSFT", "AMZN", "META", "GOOG", "AVGO", "WMT", "NFLX", "MA", "V", "LLY", "ORCL"}
+	NumTraders                 = len(TICKERS)
+	timeBetweenTrades          = TimeWindow.Seconds() / NUM_TIMES_TRADE_PER_WINDOW
+	timeScale                  = time.Second
+)
+
+func main() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		fmt.Println("Could not load env vars")
+		return
+	}
+	apcaKey := os.Getenv("APCA_API_KEY_ID")
+	apcaSecret := os.Getenv("APCA_API_SECRET_KEY")
+	baseURL := os.Getenv("APCA_API_BASE_URL")
+
+	dataClient := marketdata.NewClient(marketdata.ClientOpts{
+		APIKey:    apcaKey,
+		APISecret: apcaSecret,
+		// BaseURL:   base_url,
+		Feed: marketdata.IEX,
+	})
+	tradeClient := alpaca.NewClient(alpaca.ClientOpts{
+		APIKey:    apcaKey,
+		APISecret: apcaSecret,
+		BaseURL:   baseURL,
+	})
+	rmdc := RunningMeanDataClient{dataClient: dataClient, meanPeriods: 10 * time.Minute, timeWindow: TimeWindow}
+
+	// trades, rmdc_err := rmdc.GetTrades("AAPL")
+	// if rmdc_err != nil {
+	//		fmt.Println("There's an error:")
+	//		fmt.Println(rmdc_err)
+	//	}
+	//	fmt.Println(trades)
+	fwts := make([]*FixedWindowTrader, NumTraders)
+
+	// making traders
+	for i, t := range TICKERS {
+		fwt, fwtErr := NewFixedWindowTrader(t, SlopeTarget{target: 0, timeScale: TimeWindow}, &rmdc, tradeClient)
+		if fwtErr != nil {
+			fmt.Println("FixedWindowTrader instantiation error")
+			fmt.Println(fwtErr)
+		} else {
+			fwts[i] = fwt
+		}
+
 	}
 
+	for _, fwt := range fwts {
+		go RunFixedWindowTrader(fwt)
+	}
+	<-exitChannel
 }
